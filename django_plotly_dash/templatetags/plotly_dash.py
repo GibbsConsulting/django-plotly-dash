2f0d77b4a1ef31b0fc9051a431a54f0e8437b55c
Mark Gibbs <mark@gibbs.consulting>
2018-05-11 11:23:41 -0700

    Made iframe in template be responsive

--- django_plotly_dash/templatetags/plotly_dash.py
+++ django_plotly_dash/templatetags/plotly_dash.py
@@ -5,7 +5,24 @@ register = template.Library()
 from django_plotly_dash.models import DashApp
 
 @register.inclusion_tag("django_plotly_dash/plotly_item.html", takes_context=True)
-def plotly_item(context, app_name):
+def plotly_item(context, app_name, ratio=0.1, use_frameborder=False):
+
+    fbs = use_frameborder and '1' or '0'
+
+    dstyle = """
+    position: relative;
+    padding-bottom: %s%%;
+    height: 0;
+    overflow:hidden;
+    """ % (ratio*100)
+
+    istyle = """
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    """
 
     app = DashApp.get_app_instance(app_name)
 
