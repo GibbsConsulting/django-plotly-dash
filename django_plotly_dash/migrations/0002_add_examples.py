3b85b4ee9656d9bcd0acc7961209ec88ff84ed13
Mark Gibbs <mark@gibbs.consulting>
2018-06-18 15:38:13 -0700

    Baked in second example app in migration

--- django_plotly_dash/migrations/0002_add_examples.py
+++ django_plotly_dash/migrations/0002_add_examples.py
@@ -19,8 +19,32 @@ def addExamples(apps, schema_editor):
 
     da1.save()
 
+    sa2 = StatelessApp(app_name="Connected",
+                       slug="connected")
+
+    sa2.save()
+
+    da2 = DashApp(stateless_app=sa2,
+                  instance_name="Connected-2",
+                  slug="connected-2",
+                  base_state='''{"dynamic": {"value": "Dynamo 123",
+                                             "uid": "need_to_generate_this",
+                                             "channel_name": "test_widget_channel",
+                                             "label": "rotational energy"},
+                                 "dropdown-one": {"value": "Oxygen"},
+                                 "also_dynamic": {"value": "Alternator 456",
+                                                  "uid": "and_this_one",
+                                                  "channel_name": "test_widget_channel",
+                                                  "label": "momentum"}}''',
+                  save_on_change=True)
+
+    da2.save()
+
 
 def remExamples(apps, schema_editor):
+
+    DashApp = apps.get_model("django_plotly_dash","DashApp")
+    StatelessApp = apps.get_model("django_plotly_dash","StatelessApp")
 
     DashApp.objects.all().delete()
     StatelessApp.objects.all().delete()
