2891cd46acf8670d1bc8a711b960f277edb4ed69
Mark Gibbs <mark@gibbs.consulting>
2018-07-08 23:18:22 -0700

    Test improvements

--- django_plotly_dash/tests.py
+++ django_plotly_dash/tests.py
@@ -4,6 +4,8 @@ Tests of django_plotly_dash.
 These tests can be directly used or imported into eg the tests of the demo app
 '''
 
+import pytest
+
 def test_dash_app():
     'Test the import and formation of the dash app orm wrappers'
 
@@ -12,3 +14,24 @@ def test_dash_app():
 
     assert stateless_a
     assert stateless_a.app_name
+
+def test_util_error_cases(settings):
+
+    settings.PLOTLY_DASH = None
+
+    from django_plotly_dash.util import pipe_ws_endpoint_name
+
+    # TODO provide a separate function that has settings as an argument
+    assert pipe_ws_endpoint_name() == 'ws/channel'
+
+@pytest.mark.django_db
+def test_direct_access(client):
+
+    from django.urls import reverse
+    url = reverse('the_django_plotly_dash:layout',kwargs={'ident':'SimpleExample'})
+
+    response = client.get(url)
+
+    assert response.content
+    # TODO add more tests here
+    assert False
