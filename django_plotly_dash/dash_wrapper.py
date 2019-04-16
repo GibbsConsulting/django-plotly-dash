bda1be064374953babf704f1577afa083f71c6b8
Mark Gibbs <mark@gibbs.consulting>
2019-04-16 10:50:21 -0700

    Temporary restriction to prevent use of multiple callback outputs

--- django_plotly_dash/dash_wrapper.py
+++ django_plotly_dash/dash_wrapper.py
@@ -437,7 +437,15 @@ class WrappedDash(Dash):
 
     def callback(self, output, inputs=[], state=[], events=[]): # pylint: disable=dangerous-default-value
         'Invoke callback, adjusting variable names as needed'
-        return super(WrappedDash, self).callback(self._fix_callback_item(output),
+
+        if isinstance(output, (list, tuple)):
+            fixed_outputs = [self._fix_callback_item(x) for x in output]
+            # Temporary check; can be removed once the library has been extended
+            raise NotImplementedError("django-plotly-dash cannot handle multiple callback outputs at present")
+        else:
+            fixed_outputs = self._fix_callback_item(output)
+
+        return super(WrappedDash, self).callback(fixed_outputs,
                                                  [self._fix_callback_item(x) for x in inputs],
                                                  [self._fix_callback_item(x) for x in state])
 
