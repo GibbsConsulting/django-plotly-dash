81e7e7f65002698f08ca6b0beeef30bc474a93b6
jinjun sun <jijun.sun@ga.gov.au>
2021-10-30 20:42:42 +1100

    update ignore

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -11,7 +11,23 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
+
+sentry_sdk.init(
+    dsn="https://6b4d2f8c2982454b90eb3225574da10e@o1055797.ingest.sentry.io/6041946",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
