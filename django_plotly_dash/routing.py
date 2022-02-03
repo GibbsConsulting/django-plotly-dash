59746d3b6dd7d12a8c16c9209241da5500929c55
Saran Connolly <36115026+Saran33@users.noreply.github.com>
2022-02-03 19:52:37 +0400

    Updated Routing

--- django_plotly_dash/routing.py
+++ django_plotly_dash/routing.py
@@ -22,31 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from channels.auth import AuthMiddlewareStack
-# from channels.http import AsgiHandler
-
-# #from django.conf.urls import url
-# from django.urls import re_path
-
-# from .consumers import MessageConsumer, PokePipeConsumer
-# from .util import pipe_ws_endpoint_name, http_endpoint, http_poke_endpoint_enabled
-
-# # TODO document this and discuss embedding with other routes
-
-# http_routes = [
-#     ]
-
-# if http_poke_endpoint_enabled():
-#     http_routes.append(re_path(http_endpoint("poke"), PokePipeConsumer.as_asgi()))
-
-# http_routes.append(re_path("^", AsgiHandler)) # AsgiHandler is 'the normal Django view handlers'
-
-# application = ProtocolTypeRouter({
-#     'websocket': AuthMiddlewareStack(URLRouter([re_path(pipe_ws_endpoint_name(), MessageConsumer),])),
-#     'http': AuthMiddlewareStack(URLRouter(http_routes)),
-#     })
-
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
