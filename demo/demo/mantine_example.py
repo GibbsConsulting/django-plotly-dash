f99495a413e3fb2ae7b781c3e4e123129ed81bc2
Mark Gibbs <delsim@users.noreply.github.com>
2023-03-04 21:41:28 -0800

    Extend demos (#441)
    * Add updated demo templates
    
    * Add mantine component example
    
    * Fix file header
    
    ---------
    
    Co-authored-by: delsim <dev@gibbsconsulting.ca>

---
+++ demo/demo/mantine_example.py
@@ -0,0 +1,38 @@
+'''
+Dash apps for the demonstration of functionality
+
+Copyright (c) 2023 Gibbs Consulting and others - see CONTRIBUTIONS.md
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+from dash import dcc, html
+from django_plotly_dash import DjangoDash
+
+import dash_mantine_components as dmc
+
+
+mantine_example = DjangoDash("MantineExample")
+
+
+mantine_example.layout = dmc.Alert(
+    "Hi from Dash Mantine Components. You can create some great looking dashboards using me!",
+    title="Welcome!",
+    color="violet",
+)
--- demo/demo/scaffold.py
+++ demo/demo/scaffold.py
@@ -5,10 +5,11 @@ from django.utils.module_loading import import_string
 
 from demo.plotly_apps import multiple_callbacks, flexible_expanded_callbacks
 
+
 def stateless_app_loader(app_name):
 
     # Load a stateless app
     return import_string("demo.scaffold." + app_name)
 
-demo_app = DjangoDash(name="name_of_demo_app")
 
+demo_app = DjangoDash(name="name_of_demo_app")
