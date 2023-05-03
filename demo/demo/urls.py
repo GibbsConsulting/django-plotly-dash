b22c5bbcc6a9c0d81b46bd67c913759cc560b1bf
delsim <dev@gibbsconsulting.ca>
2023-05-02 22:43:48 -0700

    Remove whitenoise use

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -18,7 +18,6 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls import url
 
 from django.views.generic import TemplateView
 
